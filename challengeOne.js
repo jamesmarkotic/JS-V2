de1693d44306bf026a75e965c9e60174ecb145dd
jamesmarkotic <jamesmarkotic@gmail.com>
2018-06-18 19:59:37 -0700

    Challenge Two

---
+++ challengeOne.js
@@ -0,0 +1,12 @@
+var game = {};
+
+game.murderer = '??';
+
+game['weapons'] = ['gun', 'statue', 'knife', 'fireplace iron', 'letter opener'];
+
+game.characters = [];
+
+game.characters[0] = ['bob'];
+game.characters.push('jim', 'paul');
+
+console.log(game);
---
+++ challengeTwo.js
@@ -0,0 +1,4 @@
+// { "name": "Rusty", "room": "kitchen", "weapon": "candlestick" }
+// const obj = {};
+
+const { name, weapon, room } = { "name": "Rusty", "room": "kitchen", "weapon": "candlestick" };
